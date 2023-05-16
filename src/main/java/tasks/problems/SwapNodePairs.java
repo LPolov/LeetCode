ef37e4b05a3f68fb6351fa4ac11f10e71fec3715
Половинко Леонтий <lpolovinko@metaprime.ru>
2023-05-16 14:20:26 +0300

    test

--- src/main/java/tasks/problems/SwapNodePairs.java
+++ src/main/java/tasks/problems/SwapNodePairs.java
@@ -1,4 +1,38 @@
 package tasks.problems;
 
+import java.util.Objects;
+
 public class SwapNodePairs {
+  public ListNode swapPairs(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode newHead = swap(head, head.next);
+    ListNode currNode = newHead.next.next;
+
+    while (currNode != null) {
+      currNode = swap(currNode, currNode.next);
+    }
+    return newHead;
+
+  }
+
+  private ListNode swap(ListNode node, ListNode next) {
+
+    if(Objects.isNull(node) || Objects.isNull(next)) {
+      return null;
+    }
+
+    ListNode newNext = new ListNode(node.val, next.next);
+    return new ListNode(next.val, newNext);
+  }
+
+  public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 }
---
+++ src/test/java/tasks/problems/SwapNodePairsTest.java
@@ -0,0 +1,11 @@
+package tasks.problems;
+
+
+import org.junit.jupiter.api.Test;
+
+class SwapNodePairsTest {
+
+  @Test
+  void swapPairs() {
+  }
+}
